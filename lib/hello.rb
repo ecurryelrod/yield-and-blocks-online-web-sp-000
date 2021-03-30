f552bdabee0a594ab85717a281e3eed67a258673
Erika Curry-Elrod <ecurryelrod@gmail.com>
2021-03-30 17:31:17 +0000

    Automatically backed up by Learn

--- lib/hello.rb
+++ lib/hello.rb
@@ -7,10 +7,15 @@ def hello_t(array)
       i = i + 1
     end
     array
-  
+  else
+    puts "Hey! No block was given!"
   end
 end
 
 # call your method here!
 
-hello_t(["Tim", "Tom", "Jim"])
+hello_t(["Tim", "Tom", "Jim"]) do |name|
+  if name.start_with?("T")
+    puts "Hi, #{name}"
+  end
+end
